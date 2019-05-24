697e86e740effb26cf1d18745080ef0f9a079a5f
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-05-24 04:13:19 -0300

    [FrontEnd] DRF + React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    products: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const products = await res.json();
+      this.setState({
+        products
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.products.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <img src={item.url} alt={item.title} />
+            <h2>{item.price}</h2>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
