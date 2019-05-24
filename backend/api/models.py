bfc3bcdf0ee383bd0193893f374e6c7b4d29f42f
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-05-24 03:02:50 -0300

    Add backend files

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    price = models.FloatField()
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.title
+
--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
-
+from .models import Product
 # Create your tests here.
+
+
+class ProductModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Product.objects.create(title="First Product", price=0)
+        Product.objects.create(url="a url here", price=0)
+        Product.objects.create(price=111.90)
+
+    def test_title_content(self):
+        print("Test Title Content")
+        product = Product.objects.get(id=1)
+        expected_object_name = f'{product.title}'
+        self.assertEquals(expected_object_name, 'First Product')
+
+    def test_url_content(self):
+        print("Test URL Content")
+        product = Product.objects.get(id=2)
+        expected_object_url = f'{product.url}'
+        self.assertEquals(expected_object_url, 'a url here')
+
+    def test_price_content(self):
+        print("Test Price Content")
+        product = Product.objects.get(id=3)
+        expected_object_price = product.price
+        self.assertEquals(expected_object_price, 111.90)
